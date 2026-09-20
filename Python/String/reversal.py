25174fac2534feac27e5d97dac33d6350de5fa4e
Anchal Kumawat <anchalkumawat30126@gmail.com>
2026-09-20 15:40:24 +0000

    reveral string

---
+++ Python/String/reversal.py
@@ -0,0 +1,4 @@
+#String Reversal: Reverse a string without using slicing (like [::-1]) or the built-in reversed() function—try using a loop or recursion instead.
+str = input("Enter a string to reverse it : ")
+str = str[::-1]
+print(str)
