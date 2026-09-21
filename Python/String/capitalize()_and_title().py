99f878a14d2931e2c7ce5e606a59cbc65a17de9d
Anchal Kumawat <anchalkumawat30126@gmail.com>
2026-09-21 08:55:17 +0000

    capitlize() and title()function use

---
+++ Python/String/capitalize()_and_title().py
@@ -0,0 +1,8 @@
+# capitlize() and title()function use 
+print("use capitlize() function :- ")
+a = input("Enter a string to capitlize it : ")
+print(a.capitalize())
+print("")
+print("use title() function :- ")
+b = input("Enter a string for title : ")
+print(b.title())
