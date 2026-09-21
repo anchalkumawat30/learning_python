ba9e4ffcf49e34d994a4c9258a124b1b0d677fbb
Anchal Kumawat <anchalkumawat30126@gmail.com>
2026-09-21 08:56:42 +0000

    use swapcase() and find() function

---
+++ Python/String/swapcase()_and_find().py
@@ -0,0 +1,10 @@
+#use swapcase() and find() function
+print("use swapcase() function :- ")
+a = input("Enter a string to swap the capital letter into samll letter and small latter to capital latter : ")
+print(a.swapcase())
+print("")
+#find() function Searches for a substring and return the position of its first oocurrence 
+print("use find function :- ")
+b = input("Enter a string : ")
+c = input ("Enter a charectar to find ")
+print(b.find(c))
