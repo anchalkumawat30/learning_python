86242ff8bdf3e5eec08d2737b9563bd4c0e8bd4b
Anchal Kumawat <anchalkumawat30126@gmail.com>
2026-09-21 08:57:17 +0000

    upper() and lower() function use

---
+++ Python/String/upper()_and_lower().py
@@ -0,0 +1,10 @@
+#upper() and lower() function use 
+#upper function 
+print("use upper() function :- ")
+a = input("Enter a string in small letter to dispaly it capital letter : ")
+print(a.upper())
+print("")
+#lower function
+print("use lower() function :- ")
+b = input("Enter a strinng in capital letter to display it small letter : ")
+print(a.lower())
